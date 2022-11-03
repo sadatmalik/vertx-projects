52e80226a49a861a1d90db251724c1dbf67a2cae
sadat.malik <sadat.malik@me.com>
2022-11-03 17:47:12 +0000

    sample db file writer

---
+++ vertx-in-action/src/main/java/chapter4_streams/db_read_write/SampleDatabaseWriter.java
@@ -0,0 +1,59 @@
+package chapter4_streams.db_read_write;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.file.AsyncFile;
+import io.vertx.core.file.OpenOptions;
+
+/**
+ * @author sm@creativefusion.net
+ */
+public class SampleDatabaseWriter {
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+
+        /*
+         * open a file
+         */
+        AsyncFile file = vertx.fileSystem().openBlocking("sample.db",
+                new OpenOptions().setWrite(true).setCreate(true));
+
+        /*
+         * append data to a buffer
+         */
+        Buffer buffer = Buffer.buffer();
+
+        // Magic number
+        buffer.appendBytes(new byte[] { 1, 2, 3, 4});
+
+        // Version
+        buffer.appendInt(2);
+
+        // DB name
+        buffer.appendString("Sample database\n");
+
+        // Entry 1
+        String key = "abc";
+        String value = "123456-abcdef";
+        buffer
+                .appendInt(key.length())
+                .appendString(key)
+                .appendInt(value.length())
+                .appendString(value);
+
+        // Entry 2
+        key = "foo@bar";
+        value = "Foo Bar Baz";
+        buffer
+                .appendInt(key.length())
+                .appendString(key)
+                .appendInt(value.length())
+                .appendString(value);
+
+        /*
+         * write the buffer to file
+         */
+        file.end(buffer, ar -> vertx.close());
+    }
+}
