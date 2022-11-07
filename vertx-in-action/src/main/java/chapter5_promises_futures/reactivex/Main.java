023b487859495e80548fc2c6fa50f6ebea67f176
sadat.malik <sadat.malik@me.com>
2022-11-07 20:44:46 +0000

    main

---
+++ vertx-in-action/src/main/java/chapter5_promises_futures/reactivex/Main.java
@@ -0,0 +1,30 @@
+package chapter5_promises_futures.reactivex;
+
+/**
+ * @author sm@creativefusion.net
+ */
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+
+        vertx.deployVerticle("chapter5_promises_futures.sensor.HeatSensor",
+                new DeploymentOptions().setConfig(new JsonObject()
+                        .put("http.port", 3000)));
+
+        vertx.deployVerticle("chapter5_promises_futures.sensor.HeatSensor",
+                new DeploymentOptions().setConfig(new JsonObject()
+                        .put("http.port", 3001)));
+
+        vertx.deployVerticle("chapter5_promises_futures.sensor.HeatSensor",
+                new DeploymentOptions().setConfig(new JsonObject()
+                        .put("http.port", 3002)));
+
+        vertx.deployVerticle("chapter5_promises_futures.snapshot.SnapshotService");
+        vertx.deployVerticle("chapter5_promises_futures.reactivex.CollectorService");
+    }
+}
