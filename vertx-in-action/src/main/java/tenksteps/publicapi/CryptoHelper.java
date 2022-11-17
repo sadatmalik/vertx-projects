79bdb08a8e2b866b213d1aa9601c4131616905ee
sadat.malik <sadat.malik@me.com>
2022-11-17 19:38:17 +0000

    crypto helper stub

---
+++ vertx-in-action/src/main/java/tenksteps/publicapi/CryptoHelper.java
@@ -0,0 +1,16 @@
+package tenksteps.publicapi;
+
+import java.io.IOException;
+
+/**
+ * @author sm@creativefusion.net
+ */
+public class CryptoHelper {
+    public static String publicKey() throws IOException {
+        return null;
+    }
+
+    public static String privateKey() {
+        return null;
+    }
+}
