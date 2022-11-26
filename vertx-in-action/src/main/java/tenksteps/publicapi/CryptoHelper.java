5348bcab3aa602700d8345c9d58eedf90b7e7137
sadat.malik <sadat.malik@me.com>
2022-11-26 13:14:24 +0000

    crypto helper

--- vertx-in-action/src/main/java/tenksteps/publicapi/CryptoHelper.java
+++ vertx-in-action/src/main/java/tenksteps/publicapi/CryptoHelper.java
@@ -1,16 +1,29 @@
 package tenksteps.publicapi;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @author sm@creativefusion.net
  */
-public class CryptoHelper {
-    public static String publicKey() throws IOException {
-        return null;
+class CryptoHelper {
+
+    static String publicKey() throws IOException {
+        return read("public_key.pem");
     }
 
-    public static String privateKey() {
-        return null;
+    static String privateKey() throws IOException {
+        return read("private_key.pem");
+    }
+
+    private static String read(String file) throws IOException {
+        Path path = Paths.get("public-api", file);
+        if (!path.toFile().exists()) {
+            path = Paths.get("..", "public-api", file);
+        }
+        return String.join("\n", Files.readAllLines(path, StandardCharsets.UTF_8));
     }
 }
